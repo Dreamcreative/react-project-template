ab5ef32d739cd83e7366c78a171c54fbbf802bb0
maxiao <m543438924@163.com>
2022-10-28 23:20:01 +0800

    chore: 配置webpack 热更新

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -1,10 +1,12 @@
 const { merge } = require('webpack-merge')
+const webpack = require('webpack')
 const webpackConfig = {
   mode: 'development',
   devServer: {
     host: 'localhost',
     hot: true,
     port: 8080
-  }
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 }
 module.exports = merge(webpackConfig, require('./webpack.common'))
