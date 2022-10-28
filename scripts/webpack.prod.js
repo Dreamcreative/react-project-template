caa9a1d7edec4c8f1b2d4f41695afcc1e3b4998c
maxiao <m543438924@163.com>
2022-10-28 23:23:01 +0800

    chore: prod 添加依赖分析

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -1,8 +1,9 @@
 const { merge } = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const webpackConfig = {
   mode: 'production',
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [new CleanWebpackPlugin(), new BundleAnalyzerPlugin()]
 }
 module.exports = merge(webpackConfig, require('./webpack.common'))
